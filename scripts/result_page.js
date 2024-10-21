0824e16f36d6cabc885f3acf351b15c321d2ef19
Valter Matta <mattavalter98@gmail.com>
2024-10-21 19:18:49 +0200

    finish for today

---
+++ scripts/result_page.js
@@ -0,0 +1,10 @@
+const esito = document.getElementById('result') // congrats
+const passed = document.getElementById('passed') // you passed exam
+const certified = document.getElementById('certificate') //paragr. certified
+const percRightAnswers = document.getElementById('win') //%
+const percWrongAnswers = document.getElementById('lose') //%
+const rightAnswers = document.getElementById('lose') //numb
+const wrongAnswers = document.getElementById('lose') //numb
+
+// div cerchio percentuale
+const divCircle = document.getElementById('graphic') //numb
